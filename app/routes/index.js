4bb4b725f1ec8873b49d34a807f8ac7afba4ba4b
Jennifer Bland <ratracegrad@gmail.com>
2016-04-18 18:12:29 -0400

    Added routes

---
+++ app/routes/index.js
@@ -0,0 +1,61 @@
+module.exports = function(app, passport) {
+
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+     app.post('/signup', passport.authenticate('local-signup', {
+         successRedirect: '/profile',
+         failureRedirect: '/signup',
+         failureFlash: true
+     }));
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+    // route middleware to make sure a user is logged in
+    function isLoggedIn(req, res, next) {
+
+        if (req.isAuthenticated()) {
+            return next();
+        }
+
+        res.redirect('/');
+}
